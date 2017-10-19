85c91eff0ffc3cea1a64bcd039263ab03e2e2ff0
sichoon.kwon <sichoon.kwon@solbox.com>
2017-10-19 23:25:31 +0900

    log view dev

--- routes/accesslog-view.js
+++ routes/accesslog-view.js
@@ -1,12 +1,24 @@
 const express = require("express");
-const router = express.Router()
+const fs = require("fs");
+const router = express.Router();
 
 function fncAccessMain(req, res) {
     res.render("accesslog", {});
 }
 
 function fncAccessData(req, res) {
+    let dateParam = req.query.dateParam;
+    let hourParam = req.query.hourParam;
+    console.log(req.query);
+    let fileName1 = "2017101919-SK-81-FHS5174.log";
+    let fileName = "acesss_" + dateParam + hourParam + ".log";
+    console.log(fileName);
+    let fileData = fs.readFileSync("../access/" + fileName1, "utf-8").split("\n");
     let arrData = [];
+
+    fileData.forEach(function (item) {
+        arrData.push({"data": item});
+    });
     res.send({"arrData":arrData});
 }
 
