ef5d3e7ad629671bd256dfd40aa2a8aa4752ec84
sichoon.kwon <sichoon.kwon@solbox.com>
2017-10-27 23:26:34 +0900

    cluster

--- wj-sync-web.js
+++ wj-sync-web.js
@@ -1,3 +1,4 @@
+const cluster = require('cluster');
 const express = require('express');
 const bodyParser = require('body-parser');
 const syncDataHandle = require('./routes/sync-data-handle');
@@ -13,27 +14,39 @@ const mp = require('./routes/main-view');
 
 const app = express();
 let port = 8888;
+const numCPUS = require('os').cpus().length;
 
-// ejs
-app.engine("html", require("ejs").renderFile);
-app.set("view engine", "ejs");
+if(cluster.isMaster) {
+    for(let i=0 ; i < numCPUS ; i++) {
+        cluster.fork();
+    }
+    cluster.on("exit", function (worker, code, signal) {
+        console.log("worker "+worker.process.pid+" died");
+    })
+} else {
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
-app.use("/sync/data", syncDataHandle);
-app.use("/view", syncView);
-app.use("/view20171026", syncView20171026);
-app.use("/viewBefore", syncViewBefore);
-app.use("/viewOld", syncViewOld);
-app.use("/before", syncBefore);
-app.use("/download", fileDw);
-app.use("/hls", hlsView);
-app.use("/public", express.static(__dirname+"/public"));
-app.use("/", mp);
-// app.use("/accesslog", accesslogView);
+    // ejs
+    app.engine("html", require("ejs").renderFile);
+    app.set("view engine", "ejs");
 
-// start server
-app.listen(port);
-console.info("##################################");
-console.log("# node js server start port:"+port +"#");
-console.info("##################################");
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: false}));
+    app.use("/sync/data", syncDataHandle);
+    app.use("/view", syncView);
+    app.use("/view20171026", syncView20171026);
+    app.use("/viewBefore", syncViewBefore);
+    app.use("/viewOld", syncViewOld);
+    app.use("/before", syncBefore);
+    app.use("/download", fileDw);
+    app.use("/hls", hlsView);
+    app.use("/public", express.static(__dirname + "/public"));
+    app.use("/", mp);
+    // app.use("/accesslog", accesslogView);
+
+    // start server
+    app.listen(port);
+    console.info("##################################");
+    console.log("# node js server start port:" + port + "#");
+    console.info("##################################");
+
+}
